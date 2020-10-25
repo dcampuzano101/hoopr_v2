dd2d2bf14cbfb7fbebe0eb47319cf8cce34a8985
dcampuzano101 <dcampuzano101@gmail.com>
2020-10-25 13:32:25 -0400

    Creates errorMiddleware

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import colors from "colors";
 import userRoutes from "./routes/userRoutes.js";
 
 import connectDB from "./config/db.js";
-// import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 // import runRoutes from "./routes/runRoutes.js";
 // import orderRoutes from "./routes/orderRoutes.js";
 
@@ -17,6 +17,8 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(notFound);
+app.use(errorHandler);
 
 app.use("/api/users", userRoutes);
 
