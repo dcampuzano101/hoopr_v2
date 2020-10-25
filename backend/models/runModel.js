7ef7414742fe7bdd21df5f8c9b13d449acc82166
dcampuzano101 <dcampuzano101@gmail.com>
2020-10-25 15:03:27 -0400

    runSchema runModel

---
+++ backend/models/runModel.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const runSchema = mongoose.model.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    capacity: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    users: {
+      type: Array,
+      required: true,
+      default: [],
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Run = mongoose.model("Run", runSchema);
+
+export default Run;
