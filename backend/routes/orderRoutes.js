90a8860bf39d3f5c27fbca73b89883e6536ee824
dcampuzano101 <dcampuzano101@gmail.com>
2020-10-25 16:15:13 -0400

    order-routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { protect, admin } from "../middleware/authMiddleware.js";
+const router = express.Router();
+
+router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
+
+router.route("/:id").get(protect, getOrderById);
+
+router.route("/myorders").get(protect, getMyOrders);
+
+router.route("/:id/pay").put(protect, updateOrderToPaid);
+
+//manually edit order
+router.route("/:id/update").put(protect, admin, updateOrder);
+
+export default router;
