462cff8a79c531a4ca8ce3648c29a5f80fdcad39
dcampuzano101 <dcampuzano101@gmail.com>
2020-10-25 13:39:55 -0400

    user-routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,3 @@
+import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
+import User from "../models/userModel.js";
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+const router = express.Router();
+
+router.route("/").post(registerUser).get(protect, admin, getUsers);
+
+router.route("/login").post(authUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route("/:id")
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
+  .delete(protect, admin, deleteUser);
