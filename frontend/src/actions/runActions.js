5c1843de514e74b2608ceeaac96487d8ebb13522
dcampuzano101 <dcampuzano101@gmail.com>
2020-10-31 14:30:37 -0400

    adds create/delete/update run actions /reducers / constants on frontend

--- frontend/src/actions/runActions.js
+++ frontend/src/actions/runActions.js
@@ -3,6 +3,18 @@ import {
   RUN_LIST_REQUEST,
   RUN_LIST_SUCCESS,
   RUN_LIST_FAIL,
+  RUN_DETAILS_REQUEST,
+  RUN_DETAILS_SUCCESS,
+  RUN_DETAILS_FAIL,
+  RUN_DELETE_REQUEST,
+  RUN_DELETE_SUCCESS,
+  RUN_DELETE_FAIL,
+  RUN_CREATE_REQUEST,
+  RUN_CREATE_SUCCESS,
+  RUN_CREATE_FAIL,
+  RUN_UPDATE_REQUEST,
+  RUN_UPDATE_SUCCESS,
+  RUN_UPDATE_FAIL,
 } from "../constants/runConstants";
 
 export const listRuns = () => async (dispatch) => {
@@ -21,6 +33,140 @@ export const listRuns = () => async (dispatch) => {
     dispatch({
       type: RUN_LIST_FAIL,
       error:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listRunDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: RUN_DETAILS_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/runs/${id}`, config);
+
+    dispatch({
+      type: RUN_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: RUN_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const createRun = (run) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: RUN_CREATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post("/api/runs/", config);
+
+    dispatch({
+      type: RUN_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: RUN_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const deleteRun = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: RUN_DELETE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.delete(`/api/runs/${id}`, config);
+
+    dispatch({
+      type: RUN_DELETE_SUCCESS,
+    });
+  } catch (error) {
+    dispatch({
+      type: RUN_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const updateRun = (run) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: RUN_UPDATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.put(`/api/runs/${run._id}`, run, config);
+
+    dispatch({
+      type: RUN_UPDATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: RUN_UPDATE_FAIL,
+      payload:
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
--- frontend/src/reducers/runReducers.js
+++ frontend/src/reducers/runReducers.js
@@ -2,6 +2,20 @@ import {
   RUN_LIST_FAIL,
   RUN_LIST_REQUEST,
   RUN_LIST_SUCCESS,
+  RUN_DETAILS_REQUEST,
+  RUN_DETAILS_SUCCESS,
+  RUN_DETAILS_FAIL,
+  RUN_DELETE_REQUEST,
+  RUN_DELETE_SUCCESS,
+  RUN_DELETE_FAIL,
+  RUN_CREATE_REQUEST,
+  RUN_CREATE_SUCCESS,
+  RUN_CREATE_FAIL,
+  RUN_CREATE_RESET,
+  RUN_UPDATE_REQUEST,
+  RUN_UPDATE_SUCCESS,
+  RUN_UPDATE_FAIL,
+  RUN_UPDATE_RESET,
 } from "../constants/runConstants";
 
 export const runListReducer = (state = { loading: true }, action) => {
@@ -12,6 +26,87 @@ export const runListReducer = (state = { loading: true }, action) => {
       return { loading: false, runs: action.payload };
     case RUN_LIST_FAIL:
       return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const runDetailsReducer = (state = { run: {} }, action) => {
+  switch (action.type) {
+    case RUN_DETAILS_REQUEST:
+      return {
+        loading: true,
+      };
+    case RUN_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        run: action.payload,
+      };
+    case RUN_DETAILS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const runCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case RUN_CREATE_REQUEST:
+      return {
+        loading: true,
+      };
+    case RUN_CREATE_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+        run: action.payload,
+      };
+    case RUN_CREATE_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case RUN_CREATE_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const runDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case RUN_DELETE_REQUEST:
+      return {
+        loading: true,
+      };
+    case RUN_DELETE_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      };
+    case RUN_DELETE_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const runUpdateReducer = (state = { run: {} }, action) => {
+  switch (action.type) {
+    case RUN_UPDATE_REQUEST:
+      return { loading: true };
+    case RUN_UPDATE_SUCCESS:
+      return { loading: false, success: true, run: action.payload };
+    case RUN_UPDATE_FAIL:
+      return { loading: false, error: action.payload };
+    case RUN_UPDATE_RESET:
+      return { product: {} };
     default:
       return state;
   }
