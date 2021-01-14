00db95843ec5f2262398807e3d2a26166268b661
dcampuzano101 <dcampuzano101@gmail.com>
2021-01-14 16:30:51 -0600

    uses isMobile hook to dynamically re-size logo

--- frontend_ts_v2/src/components/Logo.tsx
+++ frontend_ts_v2/src/components/Logo.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Typography, Grid, Container } from '@material-ui/core'
+import { Typography, useMediaQuery } from '@material-ui/core'
 import { makeStyles, Theme } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
+import { useTheme } from '@material-ui/core/styles'
+import { Menu } from '@material-ui/icons'
 
 const useStyles = makeStyles(({ palette }: Theme) => ({
   root: {
@@ -26,6 +28,10 @@ const useStyles = makeStyles(({ palette }: Theme) => ({
   },
   linkStyle: {
     textDecoration: 'none'
+  },
+  mobileLogo: {
+    fontSize: '1.8rem',
+    letterSpacing: '-1.8px'
   }
 }))
 
@@ -34,12 +40,19 @@ interface LogoProps {
 }
 
 const Logo: React.FC<LogoProps> = ({ logoText }) => {
+  const theme = useTheme()
   const classes = useStyles()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
   return (
     <div className={classes.headerWrapper}>
       <div className={classes.headerLogoWrapper}>
         <Link to="/" className={classes.linkStyle}>
-          <Typography variant="h1">{logoText}</Typography>
+          <Typography
+            variant="h1"
+            className={`${isMobile ? classes.mobileLogo : ''}`}
+          >
+            {logoText}
+          </Typography>
         </Link>
       </div>
     </div>
