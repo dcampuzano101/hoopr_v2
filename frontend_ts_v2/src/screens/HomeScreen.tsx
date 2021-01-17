93cc87b3e4f5f7f65f6f543899023f5bee52c4f3
dcampuzano101 <dcampuzano101@gmail.com>
2021-01-17 12:03:43 -0600

    Change structure of App.js -- includes AppRoutes.tsx

---
+++ frontend_ts_v2/src/screens/HomeScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Typography } from '@material-ui/core';
+
+interface HomeScreenProps {
+
+}
+
+ const HomeScreen: React.FC<HomeScreenProps> = ({}) => {
+        return (
+            <>
+                <Typography variant="h1">Home Screen</Typography>
+
+
+            </>
+        );
+}
+
+export default HomeScreen
