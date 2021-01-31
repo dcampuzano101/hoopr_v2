8a117cf43477180426f27b0984ba0ee7dc659417
David Campuzano <48020477+dcampuzano101@users.noreply.github.com>
2021-01-30 20:03:48 -0600

    redux TS runDetailsReducer

---
+++ frontend_ts_v2/src/reducers/runReducers.tsx
@@ -0,0 +1,71 @@
+import {
+    RUN_LIST_FAIL,
+    RUN_LIST_REQUEST,
+    RUN_LIST_SUCCESS,
+    RUN_DETAILS_REQUEST,
+    RUN_DETAILS_SUCCESS,
+    RUN_DETAILS_FAIL,
+    RUN_DELETE_REQUEST,
+    RUN_DELETE_SUCCESS,
+    RUN_DELETE_FAIL,
+    RUN_CREATE_REQUEST,
+    RUN_CREATE_SUCCESS,
+    RUN_CREATE_FAIL,
+    RUN_CREATE_RESET,
+    RUN_UPDATE_REQUEST,
+    RUN_UPDATE_SUCCESS,
+    RUN_UPDATE_FAIL,
+    RUN_UPDATE_RESET,
+    RUN_DETAILS_RESET,
+  } from "../constants/runConstants";
+import { Action } from 'redux'
+interface ReduxAction extends Action {
+    payload?: any
+  }
+
+
+  export const runListReducer = (state = { loading: true }, action: ReduxAction) => {
+    switch (action.type) {
+      case RUN_LIST_REQUEST:
+        return { loading: true };
+      case RUN_LIST_SUCCESS:
+        return { loading: false, runs: action.payload };
+      case RUN_LIST_FAIL:
+        return { loading: false, error: action.payload };
+      default:
+        return state;
+    }
+  };
+
+  export const runDetailsReducer = (
+    state = {
+      loading: true,
+      success: false,
+      run: {},
+    },
+    action: ReduxAction
+  ) => {
+    switch (action.type) {
+      case RUN_DETAILS_REQUEST:
+        return {
+          loading: true,
+        };
+      case RUN_DETAILS_SUCCESS:
+        return {
+          loading: false,
+          success: true,
+          run: action.payload,
+        };
+      case RUN_DETAILS_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        };
+      case RUN_DETAILS_RESET:
+        return {};
+      default:
+        return state;
+    }
+  };
+  
+ 
--- frontend_ts_v2/src/reducers/userReducers.tsx
+++ frontend_ts_v2/src/reducers/userReducers.tsx
@@ -21,6 +21,9 @@ import {
   USER_UPDATE_SUCCESS,
   USER_UPDATE_FAIL,
   USER_UPDATE_RESET,
+  USER_UPDATE_PROFILE_PHOTO_REQUEST,
+  USER_UPDATE_PROFILE_PHOTO_FAIL,
+  USER_UPDATE_PROFILE_PHOTO_SUCCESS,
 } from '../constants/userConstants'
 import { Action } from 'redux'
 import { User } from '../actions/userActions'
@@ -149,6 +152,23 @@ export const userUpdateReducer = (state = { user: {} }, action: ReduxAction) => 
       return { loading: false, success: true };
     case USER_UPDATE_FAIL:
       return { loading: false, error: action.payload };
+    case USER_UPDATE_RESET:
+      return {
+        user: {},
+      };
+    default:
+      return state;
+  }
+};
+
+export const userUpdateProfilePhotoReducer = (state = {}, action: ReduxAction) => {
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_PHOTO_REQUEST:
+      return { loading: true, success: false };
+    case USER_UPDATE_PROFILE_PHOTO_SUCCESS:
+      return { loading: false, success: true, photoUrl: action.payload };
+    case USER_UPDATE_PROFILE_PHOTO_FAIL:
+      return { loading: false, success: false, error: action.payload };
     case USER_UPDATE_RESET:
       return {
         user: {},
