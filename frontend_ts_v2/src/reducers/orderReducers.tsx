97be2c2b7184cbd34f7093794d29ed7c86f874fc
David Campuzano <48020477+dcampuzano101@users.noreply.github.com>
2021-01-31 20:43:24 -0600

    orderReducers TS redux

---
+++ frontend_ts_v2/src/reducers/orderReducers.tsx
@@ -0,0 +1,42 @@
+import {
+    ORDER_CREATE_FAIL,
+    ORDER_CREATE_SUCCESS,
+    ORDER_CREATE_REQUEST,
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_FAIL,
+    ORDER_LIST_MY_REQUEST,
+    ORDER_LIST_MY_FAIL,
+    ORDER_LIST_MY_SUCCESS,
+    ORDER_LIST_MY_RESET,
+    ORDER_CREATE_RESET,
+  } from "../constants/orderConstants";
+
+import { Action } from 'redux'
+interface ReduxAction extends Action {
+    payload?: any
+}
+
+export const orderCreateReducer = (state = { success: false }, action: ReduxAction) => {
+    switch (action.type) {
+      case ORDER_CREATE_REQUEST:
+        return {
+          loading: true,
+        };
+      case ORDER_CREATE_SUCCESS:
+        return {
+          loading: false,
+          success: true,
+          order: action.payload,
+        };
+      case ORDER_CREATE_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        };
+      case ORDER_CREATE_RESET:
+        return { success: false };
+      default:
+        return state;
+    }
+  };
