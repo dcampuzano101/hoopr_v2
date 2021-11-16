0cbd557849bb5dc02cb133ac0e3efbf965ad02bf
David Campuzano <dcampuzano101@gmail.com>
2021-11-15 21:26:12 -0500

    resized layout

--- frontend_react/src/components/RunScreen.js
+++ frontend_react/src/components/RunScreen.js
@@ -3,6 +3,8 @@ import React, { useEffect } from 'react';
 // import { Typography, Grid, Avatar, IconButton } from '@material-ui/core/';
 import GoogleMap from './GoogleMap';
 import { getRunDetails, getUsersForRun } from '../actions/runActions';
+import { MdShoppingCart } from 'react-icons/md';
+import { IconContext } from 'react-icons';
 import { useSelector, useDispatch } from 'react-redux';
 // import { RunDetailsState, UsersState } from '../../reducers/runReducers';
 // import { Run } from './Runs';
@@ -263,17 +265,14 @@ const RunScreen = ({ params }) => {
               screen={true}
             />
           </div>
-          <div className='w-1/2 h-full flex flex-wrap flex-row p-2'>
-            <div className='h-1/2 w-full'>
-              <div item xs={12} style={{ marginBottom: '10px' }}>
-                <h1
-                  variant='h1'
-                  className='font-rubik text-2xl sm:text-lg uppercase tracking-tight font-medium leading-6 w-1/2'
-                >
+          <div className='w-1/2 h-full flex flex-wrap flex-row'>
+            <div className='h-2/3 w-full'>
+              <div className='flex justify-center'>
+                <h1 className='font-rubik xl:text-2xl sm:text-lg uppercase tracking-tight font-medium leading-6'>
                   Players
                 </h1>
               </div>
-              <div className='grid grid-cols-5 gap-3 place-items-center'>
+              <div className='grid grid-cols-3 gap-3 place-items-center'>
                 {runUsers?.map((user, idx) => (
                   <div className=''>
                     <div
@@ -287,83 +286,89 @@ const RunScreen = ({ params }) => {
                 ))}
               </div>
             </div>
-            <div className='h-1/2 w-full'>
-              <div item xs={12} style={{ marginBottom: '10px' }}>
-                <h1
-                  variant='h1'
-                  className='font-rubik text-2xl sm:text-lg uppercase tracking-tight font-medium leading-6 w-1/2'
-                >
+            <div className='h-1/3 w-full'>
+              <div className='flex justify-center'>
+                <h1 className='font-rubik xl:text-2xl sm:text-lg uppercase tracking-tight font-medium leading-6'>
                   Run Details
                 </h1>
               </div>
-              <div>
-                <div className='{classes.infoLine}'>
-                  <div className='flex flex-row lg:w-1/2 sm:w-full p-2'>
-                    <h1
-                      variant='h1'
-                      className='font-rubik text-2xl sm:text-lg lowercase tracking-tight font-medium leading-6 w-1/2'
-                    >
-                      location:
-                    </h1>
+              <div className='flex flex-col items-center justify-center h-3/5'>
+                <div className='flex flex-row lg:w-1/2 sm:w-full p-2'>
+                  <h1
+                    variant='h1'
+                    className='font-rubik text-2xl sm:text-lg lowercase tracking-tight font-medium leading-6 w-1/2'
+                  >
+                    location:
+                  </h1>
 
-                    <h3
-                      variant='h3'
-                      className='font-roboto text-xl sm:text-base tracking-tight leading-6 w-1/2 flex items-center'
-                    >
-                      {run.location}
-                    </h3>
-                  </div>
-                  <div className='flex flex-row  lg:w-1/2 sm:w-full p-2'>
-                    <h1
-                      variant='h1'
-                      className='font-rubik text-2xl sm:text-lg lowercase tracking-tight font-medium leading-6 w-1/2'
-                    >
-                      date:
-                    </h1>
+                  <h3
+                    variant='h3'
+                    className='font-roboto text-xl sm:text-base tracking-tight leading-6 w-1/2 flex items-center'
+                  >
+                    {run.location}
+                  </h3>
+                </div>
+                <div className='flex flex-row  lg:w-1/2 sm:w-full p-2'>
+                  <h1
+                    variant='h1'
+                    className='font-rubik text-2xl sm:text-lg lowercase tracking-tight font-medium leading-6 w-1/2'
+                  >
+                    date:
+                  </h1>
 
-                    <h3
-                      variant='h3'
-                      className='font-roboto text-xl sm:text-base tracking-tight leading-6 w-1/2 flex items-center'
-                    >
-                      {run.date}
-                    </h3>
-                  </div>
-                  <div className='flex flex-row  lg:w-1/2 sm:w-full p-2'>
-                    <h1
-                      variant='h1'
-                      className='font-rubik text-2xl sm:text-lg lowercase tracking-tight font-medium leading-6 w-1/2'
-                    >
-                      price:
-                    </h1>
+                  <h3
+                    variant='h3'
+                    className='font-roboto text-xl sm:text-base tracking-tight leading-6 w-1/2 flex items-center'
+                  >
+                    {run.date}
+                  </h3>
+                </div>
+                <div className='flex flex-row  lg:w-1/2 sm:w-full p-2'>
+                  <h1
+                    variant='h1'
+                    className='font-rubik text-2xl sm:text-lg lowercase tracking-tight font-medium leading-6 w-1/2'
+                  >
+                    price:
+                  </h1>
 
-                    <h3
-                      variant='h3'
-                      className='font-roboto text-xl sm:text-base tracking-tight leading-6 w-1/2 flex items-center'
-                    >
-                      ${run.price}
-                    </h3>
-                  </div>
-                  <div className='flex flex-row  lg:w-1/2 sm:w-full p-2'>
-                    <h1
-                      variant='h1'
-                      className='font-rubik text-2xl sm:text-lg lowercase tracking-tight font-medium leading-6 w-1/2'
-                    >
-                      spots:
-                    </h1>
+                  <h3
+                    variant='h3'
+                    className='font-roboto text-xl sm:text-base tracking-tight leading-6 w-1/2 flex items-center'
+                  >
+                    ${run.price}
+                  </h3>
+                </div>
+                <div className='flex flex-row  lg:w-1/2 sm:w-full p-2'>
+                  <h1
+                    variant='h1'
+                    className='font-rubik text-2xl sm:text-lg lowercase tracking-tight font-medium leading-6 w-1/2'
+                  >
+                    spots:
+                  </h1>
 
-                    <h3
-                      variant='h3'
-                      className='font-roboto text-xl sm:text-base tracking-tight leading-6 w-1/2 flex items-center'
-                    >
-                      {run.users.length}/{run.capacity}
-                    </h3>
-                  </div>
+                  <h3
+                    variant='h3'
+                    className='font-roboto text-xl sm:text-base tracking-tight leading-6 w-1/2 flex items-center'
+                  >
+                    {run.users.length}/{run.capacity}
+                  </h3>
                 </div>
               </div>
-              <div item xs={7} md={12} className='{classes.buttonWrapper}'>
-                <h3 variant='h3' className='{classes.buttonText}'>
-                  SHOPPING_CART_ICON_ADD TO CART
-                </h3>
+              <div className='w-full flex justify-center p-2 sm:items-center'>
+                <div className='transition-all duration-300 lg:w-1/2 sm:w-4/6 sm:h-12 sm:items-center flex flex-row align-middle justify-center cursor-pointer bg-transparent hover:bg-gray-900 font-semibold hover:text-white lg:py-2 lg:px-4 border border-black hover:border-transparent rounded'>
+                  <IconContext.Provider
+                    value={{ style: { fontSize: '1.7rem' } }}
+                  >
+                    <MdShoppingCart />
+                  </IconContext.Provider>
+
+                  <h3
+                    variant='h3'
+                    className='font-roboto tracking-tight leading-6 p-1 sm:text-sm'
+                  >
+                    ADD TO CART
+                  </h3>
+                </div>
               </div>
             </div>
           </div>
