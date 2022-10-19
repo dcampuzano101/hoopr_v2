ecf72c46095a34f98751b59f4cc6761c8bb267da
David Campuzano <dcampuzano101@gmail.com>
2022-10-19 12:48:26 -0500

    fixes height problem in Runs component

--- frontend_react/src/components/Footer.js
+++ frontend_react/src/components/Footer.js
@@ -3,7 +3,7 @@ import { SocialIcon } from "react-social-icons";
 
 const Footer = () => {
   return (
-    <div className='h-20 w-auto border-black border-t-1 flex justify-center'>
+    <div className='lg:h-32 xl:h-20 w-auto border-black border-t-1 flex justify-center'>
       <div className='flex flex-row justify-evenly w-1/2 items-center'>
         <SocialIcon url='https://instagram.com/merkyoass' />
         <SocialIcon url='https://linkedin.com/in/david-campuzano' />
